1577aca5fab793b00d9dc0c262a874e06ce28c1a
JLafazan <jeffandcassy1@gmail.com>
2018-04-22 17:57:50 -0700

    Updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,29 +1,40 @@
-
-
-
-
 var friendsData =
 
 
 
- [
-{
-  "name":"Ahmed",
-  "photo":"https://www.pexels.com/photo/adult-attractive-beautiful-beauty-415829/",
-  "scores":[
-      "5",
-      "1",
-      "4",
-      "4",
-      "5",
-      "1",
-      "2",
-      "5",
-      "4",
-      "1"
-    ]
-  }
-]
+    [{
+            "name": "Ahmed",
+            "photo": "https://www.pexels.com/photo/adult-attractive-beautiful-beauty-415829/",
+            "scores": [
+                "5",
+                "1",
+                "4",
+                "4",
+                "5",
+                "1",
+                "2",
+                "5",
+                "4",
+                "1"
+            ]
+        },
 
+        {
+            "name": "Bobette",
+            "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCNf0gKcQ_7vP94scaicoSWygm4b1qzOBej5sS97h63T2FqEPX",
+            "scores": [
+                "3",
+                "3",
+                "3",
+                "3",
+                "3",
+                "3",
+                "3",
+                "3",
+                "3",
+                "3"
+            ]
+        }
+    ]
 
 module.exports = friendsData;
